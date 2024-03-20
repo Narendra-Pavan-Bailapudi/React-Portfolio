8e2d46e59181ef09cdd227e5f23c34bc00fdda6e
sbailapudi <68215997+sbailapudi@users.noreply.github.com>
2024-03-19 17:04:35 -0700

    React based Portfolio

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-
-import './App.css';
+import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './Pages/Home/Homescreen';
-import Navbar from './Pages/Home/Navbar';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Pages/Home/Navbar";
+import Home from "./Pages/Home/Homescreen";
+
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
-          <Navbar/>
+          <Navbar />
           <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='*' element={<div>404 Not Found</div>}></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<div>404 Not Found</div>}></Route>
           </Routes>
         </div>
       </Router>
